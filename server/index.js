dde0abfb409d71c58d62ad20a8ba6dd44b5d82d9
rowjay007 <rowlives@gmail.com>
2021-08-11 10:21:54 +0100

    create server with environment variables

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
